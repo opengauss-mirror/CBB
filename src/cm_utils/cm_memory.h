1b209780d45a00306586dc309abd96143ee8380b
yanghaiyan <chnhyyang@163.com>
2024-09-18 06:33:11 +0000

    update src/cm_utils/cm_memory.h.
    Signed-off-by: yanghaiyan <chnhyyang@163.com>

--- src/cm_utils/cm_memory.h
+++ src/cm_utils/cm_memory.h
@@ -141,10 +141,12 @@ typedef struct st_mem_pool {
     spinlock_t lock;
     cm_event_t event;
     bilist_t mem_zone_lst; // mem zone list
+    cm_memory_allocator_t mem_allocator;    
     CM_MAGIC_DECLARE
 } mem_pool_t;
 
 status_t buddy_pool_init(char *pool_name, uint64 init_size, uint64 max_size, mem_pool_t *mem);
+status_t buddy_pool_set_mem_allocator(mem_pool_t *mem, cm_memory_allocator_t *mem_allocator);
 void *galloc(uint64 size, mem_pool_t *mem);
 void *galloc_timeout(uint64 size, mem_pool_t *mem, uint32 timeout_ms);
 void *grealloc(void *p, uint64 size, mem_pool_t *mem);
